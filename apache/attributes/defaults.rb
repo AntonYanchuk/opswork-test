bebedbfdea407c60ea6906b7cf9009b67e63b4a9
Anton Yanchuk <anton_yanchuk@epam.com>
2020-03-18 11:29:15 +0300

    second day

--- apache/attributes/defaults.rb
+++ apache/attributes/defaults.rb
@@ -3,8 +3,10 @@ file = File.open("/etc/motd.opsworks-static")
 
 if File.readlines("/etc/motd.opsworks-static").grep(/stack1/).size > 0
     stack_name = 'stack1'
+    puts stack_name
 elsif File.readlines("/etc/motd.opsworks-static").grep(/horizon-lab3b/).size > 0
     stack_name = 'horizon-lab3b'
+    puts stack_name
 end
 
 
